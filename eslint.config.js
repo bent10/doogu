df925fd6626b516493442f51d7427e7eb8c07526
bent10 <bent10@stilearning.com>
2024-08-27 15:43:38 +0700

    feat(eslint): switch to ESLint's flat config format
    BREAKING CHANGE: See [eslint
    migrationguide](https://eslint.org/docs/latest/use/configure/migration-guide)

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import eslint from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
+
+/** @type {import('typescript-eslint').ConfigWithExtends} */
+const customConfig = {
+  ignores: ['**/coverage', '**/dist', '**/vendor']
+}
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  customConfig
+)
