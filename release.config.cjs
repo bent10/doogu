ccb7f8202a3afd735e2f4c77e2d226869c785856
bent10 <bent10@stilearning.com>
2024-08-27 22:12:09 +0700

    fix: add `release.config.cjs`

---
+++ release.config.cjs
@@ -0,0 +1,40 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+// eslint-disable-next-line no-undef
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    {
+      name: 'beta',
+      prerelease: true
+    },
+    {
+      name: 'alpha',
+      prerelease: true
+    }
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'changelog.md'
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['changelog.md'],
+        message:
+          'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}'
+      }
+    ],
+    '@semantic-release/github',
+    '@semantic-release/npm'
+  ]
+}
