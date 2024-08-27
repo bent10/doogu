a88398e818e73ba13c0f11c0fc62848fd6a7f44e
bent10 <bent10@stilearning.com>
2024-08-27 16:06:58 +0700

    fix(prettier): mv `prettier.json` to `prettier.config.js`

---
+++ prettier.config.js
@@ -0,0 +1,24 @@
+export default {
+  arrowParens: 'avoid',
+  jsxSingleQuote: true,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  vueIndentScriptAndStyle: false,
+  overrides: [
+    {
+      files: ['*.html'],
+      options: {
+        singleQuote: false,
+        htmlWhitespaceSensitivity: 'css'
+      }
+    },
+    {
+      files: ['*.scss'],
+      options: {
+        singleQuote: false
+      }
+    }
+  ]
+}
